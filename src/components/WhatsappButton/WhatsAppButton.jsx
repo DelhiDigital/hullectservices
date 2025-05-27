9c24f0d905389e55eb7ec92af21de3c5d25b423c
DelhiDigital <delhidigitalco@gmail.com>
2025-05-27 22:03:15 +0530

    first commit3

--- src/components/WhatsappButton/WhatsAppButton.jsx
+++ src/components/WhatsappButton/WhatsAppButton.jsx
@@ -3,7 +3,7 @@ import "./WhatsAppButton.css"
 
 const WhatsAppButton = () => {
   // Replace with your actual WhatsApp number
-  const whatsappNumber = "+917042034792"
+  const whatsappNumber = "+919120018844"
   const message = "Hello, I'm interested in immigration services."
 
   const handleClick = () => {
